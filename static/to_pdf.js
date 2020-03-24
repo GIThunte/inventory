2ccf2552e9ec9baede2c2e438deea467d93b6947
alex.zubov <alex.zubov@sintez.co.za>
2020-03-24 15:46:35 +0200

    Fixed pdf download

--- static/to_pdf.js
+++ static/to_pdf.js
@@ -1,14 +1,31 @@
 function Export(element_id, pdf_name) {
-    html2canvas(document.getElementById(element_id), {
-        onrendered: function (canvas) {
-            var data = canvas.toDataURL();
-            var docDefinition = {
-                content: [{
-                    image: data,
-                    width: 500
-                }]
-            };
-            pdfMake.createPdf(docDefinition).download(pdf_name);
+  html2canvas(document.getElementById(element_id), {
+        onrendered:function(canvas) {
+
+            var contentWidth  = canvas.width;
+            var contentHeight = canvas.height;
+            var pageHeight    = contentWidth / 592.28 * 841.89;
+            var leftHeight    = contentHeight;
+            var position      = 0;
+            var imgWidth      = 595.28;
+            var imgHeight     = 592.28/contentWidth * contentHeight;
+            var pageData      = canvas.toDataURL('image/jpeg', 1.0);
+            var pdf           = new jsPDF('', 'pt', 'a4');
+
+            if (leftHeight < pageHeight) {
+                pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+            } else {
+                while(leftHeight > 0) {
+                    pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+                    leftHeight -= pageHeight;
+                    position -= 841.89;
+                   
+                    if(leftHeight > 0) {
+                        pdf.addPage();
+                    }
+                }
+            }
+            pdf.save(pdf_name);
         }
     });
 }
